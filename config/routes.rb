00f89fcc461db8f0e11e59f7eff2807cbb43ff7d
Mark Orr <mark.j.l.orr@googlemail.com>
2020-06-02 20:41:08 +0100

    Basic tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
-  %w[home].each do |page|
+  %w[about archive home info register].each do |page|
     get page => "pages##{page}"
   end
 end
--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,13 +1,38 @@
 require 'rails_helper'
 
 describe PagesController do
-  before(:each) do
-    visit home_path
+  context "about" do
+    it "show" do
+      visit about_path
+      expect(page).to have_title t("about.title")
+    end
+  end
+
+  context "archive" do
+    it "show" do
+      visit archive_path
+      expect(page).to have_title t("archive.title")
+    end
   end
 
   context "home" do
     it "show" do
-      expect(page).to have_title t("title")
+      visit home_path
+      expect(page).to have_title t("home.title")
+    end
+  end
+
+  context "info" do
+    it "show" do
+      visit info_path
+      expect(page).to have_title t("info.title")
+    end
+  end
+
+  context "register" do
+    it "show" do
+      visit register_path
+      expect(page).to have_title t("register.title")
     end
   end
 end
