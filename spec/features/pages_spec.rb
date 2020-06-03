b5613e8b4965beeb54bd17004087fdbc4a179a0e
Mark Orr <mark.j.l.orr@googlemail.com>
2020-06-03 20:37:56 +0100

    Test the archive page links

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,39 +1,82 @@
 require 'rails_helper'
 
 describe PagesController do
-  before(:each) do
-    visit home_path
+  context "nav links" do
+    before(:each) do
+      visit home_path
+    end
+
+    it "home" do
+      expect(page).to have_title t("home.title")
+    end
+
+    it "about" do
+      click_link t("about.link")
+      expect(page).to have_title t("about.title")
+      click_link t("home.link")
+      expect(page).to have_title t("home.title")
+    end
+
+    it "archive" do
+      click_link t("archive.link")
+      expect(page).to have_title t("archive.title")
+      click_link t("home.link")
+      expect(page).to have_title t("home.title")
+    end
+
+    it "info" do
+      click_link t("info.link")
+      expect(page).to have_title t("info.title")
+      click_link t("home.link")
+      expect(page).to have_title t("home.title")
+    end
+
+    it "register" do
+      click_link t("register.link")
+      expect(page).to have_title t("register.title")
+      click_link t("home.link")
+      expect(page).to have_title t("home.title")
+    end
   end
 
-  it "home" do
-    expect(page).to have_title t("home.title")
-  end
+  context "archive" do
+    before(:each) do
+      visit archive_path
+    end
 
-  it "about" do
-    click_link t("about.link")
-    expect(page).to have_title t("about.title")
-    click_link t("home.link")
-    expect(page).to have_title t("home.title")
-  end
+    it "2013" do
+      click_link t("conference.acts", year: 2013)
+      expect(page).to have_title t("conference.title", year: 2013)
+    end
 
-  it "archive" do
-    click_link t("archive.link")
-    expect(page).to have_title t("archive.title")
-    click_link t("home.link")
-    expect(page).to have_title t("home.title")
-  end
+    it "2014" do
+      click_link t("conference.acts", year: 2014)
+      expect(page).to have_title t("conference.title", year: 2014)
+    end
 
-  it "info" do
-    click_link t("info.link")
-    expect(page).to have_title t("info.title")
-    click_link t("home.link")
-    expect(page).to have_title t("home.title")
-  end
+    it "2015" do
+      click_link t("conference.step", year: 2015)
+      expect(page).to have_title t("conference.title", year: 2015)
+    end
 
-  it "register" do
-    click_link t("register.link")
-    expect(page).to have_title t("register.title")
-    click_link t("home.link")
-    expect(page).to have_title t("home.title")
+    it "2016" do
+      click_link t("conference.step", year: 2016)
+      expect(page).to have_title t("conference.title", year: 2016)
+    end
+
+    it "2017" do
+      click_link t("conference.step", year: 2017)
+      expect(page).to have_title t("conference.title", year: 2017)
+    end
+
+    it "2018" do
+      click_link t("conference.step", year: 2018)
+      expect(page).to have_title t("conference.title", year: 2018)
+    end
+
+    it "2019" do
+      click_link t("conference.step", year: 2019)
+      expect(page).to have_title t("conference.title", year: 2019)
+    end
   end
 end
