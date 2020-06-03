e2c9906d707ee8ca95514c5d4d32d2837bd1787a
Mark Orr <mark.j.l.orr@googlemail.com>
2020-06-03 08:46:27 +0100

    Deploy a sqlite database

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@
 set :application, "step_app"
 set :repo_url, "git@bitbucket.org:sanichi/step_app.git"
 set :log_level, :info
-append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/database.yml", "config/master.key", "db/production.sqlite3"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default branch is :master
