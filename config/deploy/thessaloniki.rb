d5abff123f3abb1f237d4488fecf902fdf5dca2a
Mark Orr <mark.j.l.orr@googlemail.com>
2020-06-06 07:36:03 +0100

    Tweak

--- config/deploy/thessaloniki.rb
+++ config/deploy/thessaloniki.rb
@@ -1,4 +1,4 @@
-server "thessaloniki.markorr.net", user: "sanichi", roles: %w{web app db}
+server "thessaloniki.sanichi.me", user: "sanichi", roles: %w{web app db}
 set :deploy_to, "/var/www/step"
 set :rails_env, "production"
 
