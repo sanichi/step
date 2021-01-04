27bcc390d9beefcd14537a8bf44bd429912c7e8a
Mark Orr <mark.j.l.orr@googlemail.com>
2021-01-04 16:04:17 +0000

    Consistency tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 set :application, "step"
 set :repo_url, "git@bitbucket.org:sanichi/step.git"
-set :log_level, :info
 append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :log_level, :info
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
