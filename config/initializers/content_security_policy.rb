1f9c4cba4784fee855ea580692aa5432aeee4fb7
Mark Orr <mark.j.l.orr@googlemail.com>
2022-03-12 11:27:00 +0000

    Tweak

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -22,5 +22,5 @@
 #
 #   # Report CSP violations to a specified URI. See:
 #   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-#   # config.content_security_policy_report_only = true # twitter problem
+#   # config.content_security_policy_report_only = true # twitter problem, $.ajax problem
 # end
