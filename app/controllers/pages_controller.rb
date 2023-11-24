c8bd628722ccba4c04f63529665758853e4f7461
Mark Orr <mark.j.l.orr@googlemail.com>
2023-11-24 14:43:24 +0000

    Update to rails 7.1.2 and add skinny env page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  def env
+    dirs = `ls /home/sanichi/.passenger/native_support`
+    vers = dirs.scan(/\d*\.\d*\.\d*/)
+    @passenger_version = vers.any? ? vers.last : "not found"
+    @host = ENV["HOSTNAME"] || `hostname`.chop.sub(".local", "")
+  end
+
   def image
     @image = Image.check(params[:year], params[:dim], params[:num])
     @next = @image.next
