abb4c1c4ea12f266341c61874cc0b1febf4dacd9
Mark Orr <mark.j.l.orr@googlemail.com>
2025-01-07 16:25:07 +0000

    Swap to using importmap

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "jquery", to: "jquery3.min.js"
+pin "bootstrap", to: "bootstrap.min.js"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile for production.
+Rails.application.config.assets.precompile += %w( jquery3.min.js bootstrap.min.js )
